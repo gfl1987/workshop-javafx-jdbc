b6d41446dbdfb5d243252e2e3bfd8eae44ee951f
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2022-02-24 17:50:45 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +23,7 @@ public class Main extends Application {
 			scrollPane.setFitToWidth(true);
 			
 			//Objeto na Cena principal
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
@@ -28,6 +31,10 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
