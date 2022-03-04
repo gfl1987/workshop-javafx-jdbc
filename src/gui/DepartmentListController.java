d9895397ea4f39bdebe13425b1f655f3e65601e3
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2022-03-04 10:28:48 -0300

    Department Service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable { 
+	
+	private DepartmentService service;
 
 	//Inicio das referencias para os componentes da tela
 	
@@ -28,6 +34,8 @@ public class DepartmentListController implements Initializable {
 
 	@FXML
 	private Button btNew; //referencia do botão
+	
+	private ObservableList<Department> obsList;
 
 	@FXML
 	public void onBtNewAction() {
@@ -35,6 +43,10 @@ public class DepartmentListController implements Initializable {
 		
 	//Fim das referencias para os componentes da tela
 
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@Override
@@ -48,6 +60,15 @@ public class DepartmentListController implements Initializable {
 		
 		Stage stage = (Stage) Main.getMainScene().getWindow(); //referencia que redimenciona a tabela de acordo com a janela
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());		
+	}
+	
+	public void updateTableView() { //método para atualizar os dados
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 
 }
